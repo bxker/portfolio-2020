f48306fca4e8e4690f245ca3297e8e4bab668a6d
bxker <jacob@Jacobs-MacBook-Pro.local>
2020-01-13 13:25:04 -0600

    added photo and changed up routes

--- src/Components/Nav/Nav.js
+++ src/Components/Nav/Nav.js
@@ -6,8 +6,7 @@ export default function Nav() {
     return (
         <div className="nav-container">
             <section>
-                <Link to="/" style={{textDecoration: 'none'}}><button>Home</button></Link>
-                <Link to="/about" style={{textDecoration: 'none'}}><button>About</button></Link>
+                <Link to="/" style={{textDecoration: 'none'}}><button>About</button></Link>
                 <Link to="/projects" style={{textDecoration: 'none'}}><button>Projects</button></Link>
                 <Link to="/contact" style={{textDecoration: 'none'}}><button>Contact</button></Link>
             </section>
