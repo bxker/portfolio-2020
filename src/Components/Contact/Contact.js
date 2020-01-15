830fc85848bf1a08967509fc86b7becf01d70e46
bxker <jacob@Jacobs-MacBook-Pro.local>
2020-01-15 14:13:56 -0600

    finished first iteration of contact page

--- src/Components/Contact/Contact.js
+++ src/Components/Contact/Contact.js
@@ -5,7 +5,19 @@ export default function Contact() {
     return (
         <div className="contact-main">
             <section className="contact-container">
-
+                <div className="social-links">
+                    <h1>Connect With Me</h1>
+                    <div>
+                        <a href="https://linkedin.com/in/bxker" target="__blank"><h2>LinkedIn</h2></a>
+                        <a href="https://github.com/bxker" target="__blank"><h2>GitHub</h2></a>
+                        <a href="https://dev.to/bxker" target="__blank"><h2>Dev.to</h2></a>
+                    </div>
+                </div>
+                <div className="contact-email">
+                    <h1>Email me!</h1>
+                    <p>Reach out to me with any questions or inquiries.</p>
+                    <a href="mailto:hello@jacob.digital"><button>hello@jacob.digital</button></a>
+                </div>
             </section>
         </div>
     )
