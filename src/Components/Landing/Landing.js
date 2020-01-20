8e7aa65f555b2faae8a5bd0ec4fe9b5757492be3
bxker <jake.luke.baker@gmail.com>
2020-01-20 15:47:20 -0600

    added resume

--- src/Components/Landing/Landing.js
+++ src/Components/Landing/Landing.js
@@ -10,6 +10,7 @@ export default function Landing() {
                 <section className="landing-section-1">
                     <img className="landing-logo" src="https://i.imgur.com/w1WnMcZ.jpg" alt="logo"></img>
                     <p>My name is <a href="https://github.com/bxker" style={{color: 'red', fontWeight: '600'}} target="__blank">Jacob Baker</a> and I am a full-stack web developer. I received a B.S. in Computer Information Technology from <a href="https://www.wku.edu/cit/" style={{color: 'red', fontWeight: '600'}} target="__blank">Western Kentucky University</a> and recently finished studying full-stack web development at <a href="https://www.devmountain.com" style={{color: '#2AABE2', fontWeight: '600'}} target="__blank">DevMountain</a> in Dallas, TX. I am currently a Mentor at <a href="https://www.devmountain.com" style={{color: '#2AABE2', fontWeight: '600'}} target="__blank">DevMountain</a>, helping students learn and grow as developers.</p>
+                    <a href="https://drive.google.com/file/d/11wHbi8Mk6Mv7TnaRKMOONLC9GaT-Y432/view?usp=sharing" target="__blank"><button className="resume-button">Resume</button></a>
                 </section>
             </div>
         </div>
